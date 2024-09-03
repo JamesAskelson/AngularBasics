5928b17b05cb434dd3c05bb8f707f07e383789d9
JamesAskelson <jamesaskelson630@gmail.com>
2024-09-03 17:21:17 -0500

    learning to bind values and set default values, along with handy form modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppNavBar } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppNavBar, HeaderComponent],
+  imports: [RouterOutlet, AppNavBar, HeaderComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
 })
+
 export class AppComponent {
   title = 'AngularBasics';
   message = 'Learning to love Angular'
   imgURL: string = 'https://aws-starter-bucket123.s3.amazonaws.com/jif-u-can-posts-urls/seedgraphic6.jpeg'
   isDisabled: boolean = false;
   isActive: boolean = true;
+
+  userName: string = 'JamesAsk'
 
   buttonClick(){
     console.log('Button click')
@@ -30,5 +35,9 @@ export class AppComponent {
 
   keyUpFiltering(user:HTMLInputElement){
     console.log(user.id)
+  }
+
+  updateUserName(username:HTMLInputElement){
+    this.userName = username.value;
   }
 }
